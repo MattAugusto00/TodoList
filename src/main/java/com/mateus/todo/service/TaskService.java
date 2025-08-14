7f30c6a08f955161f15e3c8157d565a46d608870
Mateus <mateus.augusto9@outlook.com>
2025-08-14 01:47:47 -0300

    Adicionado o TaskService

---
+++ src/main/java/com/mateus/todo/service/TaskService.java
@@ -0,0 +1,13 @@
+package com.mateus.todo.service;
+
+import com.mateus.todo.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+    private final TaskRepository repository;
+
+
+}
