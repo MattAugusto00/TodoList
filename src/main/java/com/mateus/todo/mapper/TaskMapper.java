94bc47a18f78c5dee6f2057763c9125dfc272f46
Mateus <mateus.augusto9@outlook.com>
2025-08-14 02:25:14 -0300

    Adicionado o TaskMapper

---
+++ src/main/java/com/mateus/todo/mapper/TaskMapper.java
@@ -0,0 +1,29 @@
+package com.mateus.todo.mapper;
+
+import com.mateus.todo.dto.TaskRequest;
+import com.mateus.todo.dto.TaskResponse;
+import com.mateus.todo.enums.StatusTarefa;
+import com.mateus.todo.model.Task;
+
+public class TaskMapper {
+    public static Task toEntity(TaskRequest taskRequest){
+        Task task = new Task();
+        task.setTitulo(taskRequest.titulo());
+        task.setDescricao(taskRequest.descricao());
+        task.setDataVencimento(taskRequest.dataVencimento());
+        task.setStatusTarefa(StatusTarefa.PENDENDE);
+
+        return task;
+    }
+
+    public static TaskResponse toResponse(Task task){
+        return new TaskResponse(
+                task.getUuid(),
+                task.getTitulo(),
+                task.getDescricao(),
+                task.getStatusTarefa(),
+                task.getDataCriacao(),
+                task.getDataVencimento()
+        );
+    }
+}
