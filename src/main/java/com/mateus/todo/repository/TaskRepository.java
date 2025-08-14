3a417f725ae2c3717332fdea3963ddce9aadaefd
Mateus <mateus.augusto9@outlook.com>
2025-08-14 02:56:54 -0300

    Adicionado o metodo findByTituloAndDataVencimento no repository

--- src/main/java/com/mateus/todo/repository/TaskRepository.java
+++ src/main/java/com/mateus/todo/repository/TaskRepository.java
@@ -4,8 +4,11 @@ import com.mateus.todo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByTituloAndDataVencimento(String titulo, LocalDate dataVencimento);
 }
