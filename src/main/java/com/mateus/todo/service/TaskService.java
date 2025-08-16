b970804d58ef1e9aa9d4b8ab957cac9ced0d2383
Mateus <mateus.augusto9@outlook.com>
2025-08-16 03:26:30 -0300

    Correção nas portas do application e docker-compose

--- src/main/java/com/mateus/todo/service/TaskService.java
+++ src/main/java/com/mateus/todo/service/TaskService.java
@@ -7,12 +7,14 @@ import com.mateus.todo.model.Task;
 import com.mateus.todo.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
     private final TaskRepository repository;
 
+    @Transactional
     public TaskResponse salvar(TaskRequest taskRequest){
         boolean existe = repository.findByTituloAndDataVencimento(taskRequest.titulo(),taskRequest.dataVencimento())
                 .stream()
